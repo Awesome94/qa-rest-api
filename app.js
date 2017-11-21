52467f7d122bd3c71ed26afafa2ed9f8b3743aed
David Kironde <david.kironde@andela.com>
2017-11-21 11:59:54 +0300

    add graceful error handling 404

--- app.js
+++ app.js
@@ -3,15 +3,30 @@
 var express = require('express');
 var app = express(); 
 var routes = require("./routes");
+var logger = require('morgan')
 
 var jsonParser = require("body-parser").json;
 
+app.use(logger);
 app.use('/questions', routes);
 
 app.use(jsonParser())
 
-app.use('/', function(req, res, next){
-    next();
+//catch 404 and forward to the error handler.
+app.use(function(req, res, next){
+    var err = new Error("Not found")
+    err.status  = 404;
+    next(err)
+})
+
+//error handler
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        error: {
+       message: error.message 
+        }
+    })
 })
 
 var port = process.env.PORT || 3000;
